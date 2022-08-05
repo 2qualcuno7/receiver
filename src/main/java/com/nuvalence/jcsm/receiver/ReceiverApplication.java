be94276aec9fa24090c679093187729cb9efc247
2qualcuno7 <suarezmjuan@gmail.com>
2022-08-05 09:35:12 -0500

    Dockerfile added

--- src/main/java/com/nuvalence/jcsm/receiver/ReceiverApplication.java
+++ src/main/java/com/nuvalence/jcsm/receiver/ReceiverApplication.java
@@ -3,11 +3,13 @@ package com.nuvalence.jcsm.receiver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ReceiverApplication {
 
-	@GetMapping("/")
+	@GetMapping("/poc")
 	public String home() {
 		return "Test endpoint GKE POC";
 	}
